5a2d596933f8cbb5820c00fd4703fd10312ed71a
Mark Baker-Munton <dioxic@gmail.com>
2019-02-24 14:45:09 +0000

    refactor

---
+++ src/main/java/uk/dioxic/mongotakeaway/web/ReactiveWebSocketClient.java
@@ -0,0 +1,27 @@
+package uk.dioxic.mongotakeaway.web;
+
+import org.springframework.web.reactive.socket.WebSocketMessage;
+import org.springframework.web.reactive.socket.client.ReactorNettyWebSocketClient;
+import org.springframework.web.reactive.socket.client.WebSocketClient;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+import java.time.Duration;
+
+public class ReactiveJavaClientWebSocket {
+
+    public static void main(String[] args) {
+		  
+        WebSocketClient client = new ReactorNettyWebSocketClient();
+        client.execute(
+          URI.create("ws://localhost:8080/event-emitter"),
+          session -> session.send(
+            Mono.just(session.textMessage("event-spring-reactive-client-websocket")))
+            .thenMany(session.receive()
+              .map(WebSocketMessage::getPayloadAsText)
+              .log())
+            .then())
+            .block(Duration.ofSeconds(10L));
+    }
+
+}
