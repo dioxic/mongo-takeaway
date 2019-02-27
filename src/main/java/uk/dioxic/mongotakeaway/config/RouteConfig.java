c22a92ed4cec2e8a5807eb184766b17fa1533beb
Mark Baker-Munton <dioxic@gmail.com>
2019-02-27 22:40:14 +0000

    re-enable web router to handle index page

--- src/main/java/uk/dioxic/mongotakeaway/config/RouteConfig.java
+++ src/main/java/uk/dioxic/mongotakeaway/config/RouteConfig.java
@@ -1,26 +1,24 @@
 package uk.dioxic.mongotakeaway.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
-import uk.dioxic.mongotakeaway.web.OrderHandler;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.web.reactive.function.BodyInserters.fromResource;
-import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
-@Deprecated
+@Configuration
 public class RouteConfig {
 
     @Bean
-    RouterFunction<ServerResponse> orderRoute(OrderHandler handler) {
+    RouterFunction<ServerResponse> orderRoute() {
         return route()
-                .GET("/order/{id}", accept(APPLICATION_JSON), handler::getOrder)
-                .GET("/order", accept(APPLICATION_JSON), handler::listOrders)
-                .POST("/order", handler::createOrder)
+//                .GET("/order/{id}", accept(APPLICATION_JSON), handler::getOrder)
+//                .GET("/order", accept(APPLICATION_JSON), handler::listOrders)
+//                .POST("/order", handler::createOrder)
                 .GET("/", req -> ok().body(fromResource(new ClassPathResource("static/client-websocket.html"))))
                 .GET("/react", req -> ok().body(fromResource(new ClassPathResource("static/index.html"))))
                 .build();
