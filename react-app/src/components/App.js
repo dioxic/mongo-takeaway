6a97f357c4402904dc14afe743b0f69b520d58d5
Mark Baker-Munton <dioxic@gmail.com>
2019-03-16 13:48:14 +0000

    creating order form

--- react-app/src/components/App.js
+++ react-app/src/components/App.js
@@ -4,16 +4,58 @@ import './App.css';
 import Footer from './Footer'
 import OrderForm from '../containers/OrderForm';
 import VisibleOrderList from '../containers/VisibleOrderList';
+import PropTypes from 'prop-types';
+import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-    </header>
-    <OrderForm/>
-    <VisibleOrderList />
-    <Footer />
-  </div>
-)
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    height: 140,
+    width: 100,
+  },
+  control: {
+    padding: theme.spacing.unit * 2,
+  },
+});
 
-export default App;
+class App extends React.Component {
+  state = {
+    spacing: '16',
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { spacing } = this.state;
+    return (
+      <div className="App">
+        <Grid container className={classes.root} spacing={16}>
+          <Grid item xs={12}>
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+            </header>
+          </Grid>
+          <Grid container item xs={5} justify="center">
+            <OrderForm/>
+          </Grid>
+          <Grid item xs={12}>
+            <VisibleOrderList />
+          </Grid>
+          <Grid item xs={12}>
+            <Footer />
+          </Grid>
+        </Grid>
+      </div>
+      
+    );
+  }
+}
+
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(App);
--- react-app/src/containers/OrderForm.js
+++ react-app/src/containers/OrderForm.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import { Formik } from "formik";
+import Paper from "@material-ui/core/Paper";
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
@@ -9,103 +11,184 @@ import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import { connect } from 'react-redux';
 import { saveOrder } from '../actions';
+import Typography from '@material-ui/core/Typography';
+import DateFnsUtils from '@date-io/date-fns';
+import * as Yup from "yup";
+import { MuiPickersUtilsProvider, TimePicker, DatePicker } from 'material-ui-pickers';
 
 const styles = theme => ({
+  paper: {
+    marginTop: theme.spacing.unit * 8,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    width: 'auto',
+    padding: `${theme.spacing.unit * 5}px ${theme.spacing.unit * 5}px ${theme
+      .spacing.unit * 5}px`
+  },
   container: {
-    display: 'flex',
-    flexWrap: 'wrap',
-  },
-  textField: {
-    marginLeft: theme.spacing.unit,
-    marginRight: theme.spacing.unit,
-    width: 200,
-  },
-  dense: {
-    marginTop: 19,
-  },
-  menu: {
-    width: 200,
-  },
+    maxWidth: "400px"
+  }
 });
 
+const validationSchema = Yup.object({
+  id: Yup.string("Enter a name")
+  .required("Id is required"),
+  threadId: Yup.string("Enter your email")
+    .required("threadId is required"),
+  customerId: Yup.number("Enter your customer Id")
+    .required("Enter your customer Id"),
+  state: Yup.string("Enter order status")
+    .required("Confirm order status")
+    .oneOf(["DONE", "PENDING"], "Invalid state (must be DONE or PENDING)")
+});
+
+// const styles = theme => ({
+//   card: {
+//     minWidth: 275,
+//   },
+//   textField: {
+//     marginLeft: theme.spacing.unit,
+//     marginRight: theme.spacing.unit,
+//     width: 200,
+//   },
+  // container: {
+  //   display: 'flex',
+  //   flexWrap: 'wrap',
+  // },
+  // dense: {
+  //   marginTop: 19,
+  // },
+  // menu: {
+  //   width: 200,
+  // },
+// });
+
 class OrderForm extends Component {
-  state = {
-    name: 'Cat in the Hat',
-    age: '',
-    multiline: 'Controlled',
-    currency: 'EUR',
-  };
-
-  handleChange = name => event => {
-    this.setState({ [name]: event.target.value });
-  };
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    this.props.dispatch(saveOrder(this.state));
+  constructor(props) {
+    super(props);
+    this.state = {};
   }
 
   render() {
 
     const { classes } = this.props;
+    const values = { id: "123", threadId: "", customerId: "", state: "", created: "", modified: ""};
 
     return (
-      <div>
-        <form className={classes.container} noValidate autoComplete="off" onSubmit={this.handleSubmit}>
-          <TextField
-            id="_id"
-            label="Order Id"
-            className={classes.textField}
-            value={this.state._id}
-            onChange={this.handleChange('_id')}
-            margin="normal"
-          />
-          <TextField
-            id="threadId"
-            label="Thread Id"
-            className={classes.textField}
-            value={this.state.threadId}
-            onChange={this.handleChange('threadId')}
-            margin="normal"
-          />
-          <TextField
-            id="customerId"
-            label="Customer Id"
-            className={classes.textField}
-            value={this.state.customerId}
-            onChange={this.handleChange('customerId')}
-            margin="normal"
-          />
-          <TextField
-            id="state"
-            label="Status"
-            className={classes.textField}
-            value={this.state.state}
-            onChange={this.handleChange('state')}
-            margin="normal"
-          />
-          <TextField
-            id="created"
-            label="Created Date"
-            className={classes.dateField}
-            value={this.state.created}
-            onChange={this.handleChange('created')}
-            margin="normal"
-          />
-          <TextField
-            id="modified"
-            label="Modified Date"
-            className={classes.dateField}
-            value={this.state.modified}
-            onChange={this.handleChange('modified')}
-            margin="normal"
-          />
-          <Button variant="contained" color="primary" className={classes.button} type="submit">
-            Add Order
-          </Button>
-        </form>
+      <div className={classes.container}>
+        <Paper elevation={1} className={classes.paper}>
+          <Typography className={classes.title} color="textSecondary" gutterBottom>
+            New Order
+          </Typography>
+          <Formik
+            initialValues={values}
+            validationSchema={validationSchema}
+            onSubmit={(values, { setSubmitting }) => {
+              setTimeout(() => {
+                alert(JSON.stringify(values, null, 2));
+                setSubmitting(false);
+              }, 400);
+            }}
+          >
+          {({
+              values,
+              errors,
+              touched,
+              handleSubmit,
+              handleChange,
+              handleBlur,
+              isSubmitting,
+              classes
+            }) => (
+              <form onSubmit={handleSubmit}>
+                  <TextField
+                    id="id"
+                    name="id"
+                    label="Order Id"
+                    helperText={touched.id ? errors.id : ""}
+                    error={touched.id && Boolean(errors.id)}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.id}
+                    margin='normal'
+                  />
+                  <TextField
+                    id="threadId"
+                    name="threadId"
+                    label="Thread Id"
+                    helperText={touched.threadId ? errors.threadId : ""}
+                    error={touched.threadId && Boolean(errors.threadId)}              
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.threadId}
+                    margin='normal'
+                  />
+                  <TextField
+                    id="customerId"
+                    name="customerId"
+                    label="Customer Id"
+                    helperText={touched.customerId ? errors.customerId : ""}
+                    error={touched.customerId && Boolean(errors.customerId)}              
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.customerId}
+                    margin='normal'
+                  />
+                  <TextField
+                    id="state"
+                    name="state"
+                    label="Status"
+                    helperText={touched.state ? errors.state : ""}
+                    error={touched.state && Boolean(errors.state)}              
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.state}
+                    margin='normal'
+                  />
+                  {/* <MuiPickersUtilsProvider utils={DateFnsUtils}><DatePicker
+                    id="created"
+                    label="Created Date"
+                    helperText={touched.name ? errors.name : ""}
+                    error={touched.name && Boolean(errors.name)}              
+                    format={this.props.getFormatString({
+                      dateFns: "dd/MM/yyyy",
+                    })}
+                    mask={value =>
+                      // handle clearing outside if value can be changed outside of the component
+                      value ? [/\d/, /\d/, "/", /\d/, /\d/, "/", /\d/, /\d/, /\d/, /\d/] : []
+                    }                  
+                    value={created}
+                    onChange={change.bind(null, "created")}
+                    disableOpenOnEnter
+                    animateYearScrolling={false}
+                  />
+                  <DatePicker
+                    id="modified"
+                    label="Modified Date"
+                    helperText={touched.name ? errors.name : ""}
+                    error={touched.name && Boolean(errors.name)}              
+                    format={this.props.getFormatString({
+                      dateFns: "dd/MM/yyyy",
+                    })}
+                    mask={value =>
+                      // handle clearing outside if value can be changed outside of the component
+                      value ? [/\d/, /\d/, "/", /\d/, /\d/, "/", /\d/, /\d/, /\d/, /\d/] : []
+                    }                  
+                    value={modified}
+                    onChange={change.bind(null, "modified")}
+                    disableOpenOnEnter
+                    animateYearScrolling={false}
+                  /></MuiPickersUtilsProvider> */}
+                <Button variant="contained" color="primary" type="submit" disabled={isSubmitting}>
+                  Add Order
+                </Button>
+              </form>
+          )}
+          </Formik>
+        </Paper>
       </div>
-    )
+    );
   }
 }
 
